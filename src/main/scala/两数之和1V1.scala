f6e57f6cfca591226ce702bd3618dafb731d44be
Iamshg <18852854479@163.com>
2021-09-22 11:27:30 +0900

    第一

---
+++ src/main/scala/两数之和1V1.scala
@@ -0,0 +1,27 @@
+object 两数之和1V1 {
+
+
+  def main(args: Array[String]): Unit = {
+
+
+    println("---")
+  }
+
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    var i:Int=0
+    var j = i
+    var res = List[Int]()
+    while(i<nums.length){
+      j=i+1
+      var other = target- nums(i)
+      while(j<nums.length){
+
+        if(other == nums(j))
+          res = (i::j::Nil)
+        j=j+1
+      }
+      i = i +1;
+    }
+    res.toArray
+  }
+}
